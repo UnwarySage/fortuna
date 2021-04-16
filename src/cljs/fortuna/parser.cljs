377a1679ef39895dcb5c45642cd23f5fb6ba65b9
UnwaryScholar <unwary@comcast.net>
2021-04-15 19:30:25 -0500

    parser now supplies rolls as maps
    Refactor parser definition to understand numbers

--- src/cljs/fortuna/parser.cljs
+++ src/cljs/fortuna/parser.cljs
@@ -1,26 +1,25 @@
 (ns fortuna.parser
   (:require [instaparse.core :as insta :refer-macros [defparser]]
-            [com.rpl.specter :as spc]
+            ;;[com.rpl.specter :as spc]
             [clojure.edn :as edn]))
 
 
 (def expression-parser-string
   "expression = dice-pool
   dice-pool = roll (('+'|'-'|'*') roll)*
-  roll = dice-amount <('D'|'d')> dice-sides ?[bonus] *{modifiers}
-  dice-amount = #'\\d+'
-  dice-sides = #'\\d+'
-  bonus = #'[+-]\\d+'
-  modifiers = (modifier-explode | (modifier-keep-highest | modifier-keep-lowest) | modifier-multiply-by)+
+  roll = dice-amount <('D'|'d')> dice-sides bonus? modifier-sequence?
+  dice-amount = num
+  dice-sides = num
+  bonus = num
+  modifier-sequence = modifier+
+  <modifier> = (modifier-explode | (modifier-keep-highest | modifier-keep-lowest) | modifier-multiply-by)+
   modifier-explode = <'!'>
-  modifier-keep-highest = <'kh'>#'\\d+'
-  modifier-keep-lowest = <'kl'>#'\\d+'
-  modifier-multiply-by = <'*'>#'\\d+'")
+  modifier-keep-highest = <'kh'> num
+  modifier-keep-lowest = <'kl'> num
+  modifier-multiply-by = <'*'> num
+  num = #'(\\-|\\+)?\\d+'")
 
-(def parser-transforms
-  {:dice-sides (fn [inp-num] [:dice-sides (edn/read-string inp-num)])
-   :dice-amount (fn [inp-num] [:dice-amount (edn/read-string inp-num)])
-   :bonus (fn [inp-num] [:bonus (edn/read-string inp-num)])})
+
 
 ;; Declaring the expression so that static analysis doesn't panic
 (declare expression)
@@ -28,10 +27,38 @@
 (defparser expression  expression-parser-string)
 
 
-(defn parse-expression [inp-string]
-  (insta/transform parser-transforms
-   (expression inp-string)))
-
-
 (defn parse-worked? [inp-parse]
   (not (insta/failure? inp-parse)))
+
+;; post processing functions
+(defn tag-to-pair
+  "takes a vector, and if it has one item, 
+   makes it into a vec of that item and true,
+   otherwise returns the vector"
+  [inp-vec]
+  (if (and
+       (= 1 (count inp-vec))
+       (vector? inp-vec))
+    [(first inp-vec) true]
+    inp-vec))
+
+
+(defn modifier-sequence-format
+  "takes a modifier sequence from instaparse and formats it to a vector of vectors"
+  [& modifiers]
+  {:modifier-sequence
+   (mapv tag-to-pair modifiers)})
+
+(def parser-transforms
+  {:num edn/read-string
+   :dice-sides (fn [inp-num] {:dice-sides inp-num})
+   :dice-amount (fn [inp-num] {:dice-amount inp-num})
+   :roll merge
+   :modifier-sequence modifier-sequence-format
+   :bonus (fn [inp-num] [:bonus inp-num])})
+
+
+;; final parse function
+(defn parse-expression [inp-string]
+  (insta/transform parser-transforms
+                   (expression inp-string)))
